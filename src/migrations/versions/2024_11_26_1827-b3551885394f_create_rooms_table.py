bf172ce6719f617b7988ccc6e3ca1bbdd42958be
Dergunov Ilya <keydgh@inbox.ru>
2024-11-26 18:30:59 +0300

    create table rooms

---
+++ src/migrations/versions/2024_11_26_1827-b3551885394f_create_rooms_table.py
@@ -0,0 +1,44 @@
+"""create rooms table
+
+Revision ID: b3551885394f
+Revises: 80d506ffe3e0
+Create Date: 2024-11-26 18:27:23.996188
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+# revision identifiers, used by Alembic.
+revision: str = "b3551885394f"
+down_revision: Union[str, None] = "80d506ffe3e0"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+
+    op.create_table(
+        "rooms",
+        sa.Column("id", sa.Integer(), nullable=False),
+        sa.Column("hotel_id", sa.Integer(), nullable=False),
+        sa.Column("title", sa.String(), nullable=False),
+        sa.Column("description", sa.String(), nullable=True),
+        sa.Column("price", sa.Integer(), nullable=False),
+        sa.Column("quantity", sa.Integer(), nullable=False),
+        sa.ForeignKeyConstraint(
+            ["hotel_id"],
+            ["hotels.id"],
+        ),
+        sa.PrimaryKeyConstraint("id"),
+    )
+
+
+
+def downgrade() -> None:
+
+    op.drop_table("rooms")
+
