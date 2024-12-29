69a8e0a9c3975e5d3eaa8a313b772c977a25f450
Dergunov Ilya <keydgh@inbox.ru>
2024-12-29 20:00:45 +0300

    тест регистрации юзера

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,8 +1,12 @@
 import pytest
 
+from httpx import AsyncClient, ASGITransport
+
 from src.config import settings
 from src.database import Base, engine_null_pull
+from src.main import app
 from src.models import *
+
 
 # scope="session" - чтобы фикстура выполнялась один раз за всю сессию. Если параметр не указывать, фикстура будет выполняться каждую функцию. дефолтное значение = function
 @pytest.fixture(scope="session", autouse=True)
@@ -12,8 +16,19 @@ def check_test_mode():
 
 
 @pytest.fixture(scope="session", autouse=True)
-async def async_main(check_test_mode):
+async def setup_database(check_test_mode):
     # функция check_test_mode в аргументах означает, что async_main будет выполнена после check_test_mode
     async with engine_null_pull.begin() as conn:
         await conn.run_sync(Base.metadata.drop_all)
         await conn.run_sync(Base.metadata.create_all)
+
+@pytest.fixture(scope="session", autouse=True)
+async def register_user(setup_database):
+    async with AsyncClient(transport=ASGITransport(app=app), base_url= 'http://test') as ac:
+        await ac.post(
+            '/auth/register',
+            json={
+                "email":"koskin@sup.ru",
+                "password": "124345"
+            }
+        )
--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -1,4 +1,4 @@
-from src.database import async_session_maker
+from src.database import async_session_maker_null_pull
 from src.schemas.hotels import HotelAdd
 from src.utils.db_manager import DBManager
 
@@ -6,7 +6,7 @@ from src.utils.db_manager import DBManager
 async def test_add_hotel():
     hotel_data = HotelAdd(title="Hotel_1", location="Сочинск")
 
-    async with DBManager(session_factory=async_session_maker) as db:
+    async with DBManager(session_factory=async_session_maker_null_pull) as db:
         new_hotel_data = await db.hotels.add(hotel_data)
         await db.commit()
         # print(f"{new_hotel_data=}")
