36551a1768aaa8be131fdb4110cb17df093de7d9
Dergunov Ilya <keydgh@inbox.ru>
2024-12-30 14:24:36 +0300

    дз №20 тестирование всех crud бронирований

--- tests/integration_tests/bookings/test_db.py
+++ tests/integration_tests/bookings/test_db.py
@@ -2,9 +2,7 @@ from datetime import date
 
 from src.schemas.bookings import BookingAdd
 
-
-
-async def test_add_booking(db):
+async def test_booking_crud(db):
 
     user_id = (await db.users.get_all())[0].id
     room_id = (await db.rooms.get_all())[0].id
@@ -17,5 +15,30 @@ async def test_add_booking(db):
         user_id=user_id
     )
 
-    await db.bookings.add(booking_data)
+    booking = await db.bookings.add(booking_data)
+
+    booking = await db.bookings.get_one_or_none(id = booking.id)
+    assert booking
+    print(f"{booking=}")
+
+
+    new_booking_data = BookingAdd(
+        date_from=date(year=2025, month=10, day=8),
+        date_to=date(year=2025, month=10, day=11),
+        room_id=room_id,
+        price=100,
+        user_id=user_id
+    )
+
+
+    update_booking = await db.bookings.update(new_booking_data)
+    assert update_booking
+    assert update_booking.date_from == new_booking_data.date_from
+    print(f"{update_booking=}")
+
+
+    await db.bookings.delete(id = booking.id)
+    deleted_booking = await db.bookings.get_one_or_none(id=booking.id)
+    assert deleted_booking is None
+
     await db.commit()
