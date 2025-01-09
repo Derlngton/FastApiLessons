9c93588a5a7558068dc5ff0b5cd1cd730536811b
Dergunov Ilya <keydgh@inbox.ru>
2025-01-09 17:19:15 +0300

    Параметризация теста

--- tests/integration_tests/bookings/test_api.py
+++ tests/integration_tests/bookings/test_api.py
@@ -1,22 +1,36 @@
-async def test_add_booking(db, authenticated_ac):
+import pytest
 
-    room_id = (await db.rooms.get_all())[0].id
+# параметризированный тест
+@pytest.mark.parametrize("room_id, date_from, date_to, status_code", [
+    (1,"2024-08-01","2024-08-10", 200),
+    (1,"2024-08-05","2024-08-12", 200),
+    (1,"2024-08-03","2024-08-11", 200),
+    (1,"2024-07-30","2024-08-08", 200),
+    (1,"2024-08-03","2024-08-15", 200),
+    (1,"2024-08-05","2024-08-09", 404),
+])
+async def test_add_booking(
+        room_id, date_from, date_to, status_code,
+        db,
+        authenticated_ac
+):
+
+    # room_id = (await db.rooms.get_all())[0].id
 
     response = await authenticated_ac.post(
         "/bookings",
         json={
             "room_id": room_id,
-            "date_from": "2024-08-01",
-            "date_to": "2024-08-10"
+            "date_from": date_from,
+            "date_to": date_to
         }
     )
 
-    assert response.status_code == 200
+    assert response.status_code == status_code
 
-    res = response.json()
-    # print(f"{res=}")
+    if status_code == 200:
+        res = response.json()
+        # print(f"{res=}")
 
-    assert isinstance(res, dict)
-    assert res["status"] == "ok"
-
-
+        assert isinstance(res, dict)
+        assert res["status"] == "ok"
